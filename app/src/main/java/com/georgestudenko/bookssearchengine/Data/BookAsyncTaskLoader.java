c78585231e4286fa6ed394e8e55cb6bf2be77d41
George Studenko <gnstudenko@gmail.com>
2017-04-30 00:59:24 +0200

    feat: Implement code for BookAsyncTaskLoader

--- app/src/main/java/com/georgestudenko/bookssearchengine/Data/BookAsyncTaskLoader.java
+++ app/src/main/java/com/georgestudenko/bookssearchengine/Data/BookAsyncTaskLoader.java
@@ -1,8 +1,47 @@
 package com.georgestudenko.bookssearchengine.Data;
 
+import android.support.v4.content.AsyncTaskLoader;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.georgestudenko.bookssearchengine.Models.Book;
+import com.georgestudenko.bookssearchengine.Util.BookParser;
+import com.georgestudenko.bookssearchengine.Util.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
 /**
  * Created by george on 29/04/2017.
  */
 
-public class BookAsyncTaskLoader {
+public class BookAsyncTaskLoader extends AsyncTaskLoader<List<Book>> {
+    private String mSearchTerm;
+    public BookAsyncTaskLoader(Context context, Bundle bundle) {
+        super(context);
+        mSearchTerm = bundle.getString("searchTerm");
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @Override
+    public List<Book> loadInBackground() {
+        URL url = NetworkUtils.buildUrl(mSearchTerm);
+        List<Book> list = null;
+        try {
+            String json = NetworkUtils.getResponseFromHttpUrl(url);
+            if(json!=null && json.length()>0){
+                list = BookParser.parseBooks(json);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
