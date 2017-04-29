ae284d5ff4b4724e8dade5e46eeb4bcdfb784497
George Studenko <gnstudenko@gmail.com>
2017-04-29 21:29:28 +0200

    feat: Implement LoaderManager.LoaderCallbacks on MainActivity

--- app/src/main/java/com/georgestudenko/bookssearchengine/MainActivity.java
+++ app/src/main/java/com/georgestudenko/bookssearchengine/MainActivity.java
@@ -1,13 +1,34 @@
 package com.georgestudenko.bookssearchengine;
 
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.georgestudenko.bookssearchengine.Models.Book;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>>{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+
     }
 }
