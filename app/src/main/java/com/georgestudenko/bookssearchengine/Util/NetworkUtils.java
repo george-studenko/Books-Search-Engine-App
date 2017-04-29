e6b4f128287191ac667fb73f4ad60db96c9c8fc4
George Studenko <gnstudenko@gmail.com>
2017-04-29 17:06:41 +0200

    feat: Add isConnected method checks if the user has internet connection

--- app/src/main/java/com/georgestudenko/bookssearchengine/Util/NetworkUtils.java
+++ app/src/main/java/com/georgestudenko/bookssearchengine/Util/NetworkUtils.java
@@ -47,6 +47,15 @@ public class NetworkUtils {
         }
     }
 
+    public static boolean isConnected(Context context) {
+        ConnectivityManager
+                cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null
+                && activeNetwork.isConnectedOrConnecting();
+    }
+
     public static URL buildUrl(String searchTerm){
         URL URL= null;
         Uri.Builder builder= new Uri.Builder();
