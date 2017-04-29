20e5fdef2f953045b9f36115ef62f34eb7158619
George Studenko <gnstudenko@gmail.com>
2017-04-29 21:25:28 +0200

    feat: Extend BookAdapter from ArrayAdapter<Book> implement constructor

--- app/src/main/java/com/georgestudenko/bookssearchengine/Data/BookAdapter.java
+++ app/src/main/java/com/georgestudenko/bookssearchengine/Data/BookAdapter.java
@@ -1,8 +1,34 @@
 package com.georgestudenko.bookssearchengine.Data;
 
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.georgestudenko.bookssearchengine.Models.Book;
+import com.georgestudenko.bookssearchengine.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 /**
  * Created by george on 29/04/2017.
  */
 
-public class BookAdapter {
+public class BookAdapter extends ArrayAdapter<Book> {
+    List<Book> mData;
+
+    public BookAdapter(@NonNull Context context, @NonNull List<Book> books) {
+        super(context, 0, books);
+        mData = books;
+    }
+
 }
