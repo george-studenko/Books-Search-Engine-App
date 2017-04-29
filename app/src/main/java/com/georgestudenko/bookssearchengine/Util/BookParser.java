b5012e656275696092e95a66f3545d6d06bf2e68
George Studenko <gnstudenko@gmail.com>
2017-04-29 21:27:28 +0200

    feat: Add BookParser class

---
+++ app/src/main/java/com/georgestudenko/bookssearchengine/Util/BookParser.java
@@ -0,0 +1,18 @@
+package com.georgestudenko.bookssearchengine.Util;
+
+import com.georgestudenko.bookssearchengine.Models.Book;
+
+import java.util.List;
+
+/**
+ * Created by george on 29/04/2017.
+ */
+
+public class BookParser {
+    public List<Book> parseBooks(String json){
+        List<Book> books = null;
+        
+
+        return  books;
+    }
+}
