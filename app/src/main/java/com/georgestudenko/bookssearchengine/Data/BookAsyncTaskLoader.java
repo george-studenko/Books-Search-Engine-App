ac1160b3494787144e88b56b5b1c5395dbe6e443
George Studenko <gnstudenko@gmail.com>
2017-04-29 16:27:53 +0200

    feat: Add basic project structure

---
+++ app/src/main/java/com/georgestudenko/bookssearchengine/Data/BookAsyncTaskLoader.java
@@ -0,0 +1,8 @@
+package com.georgestudenko.bookssearchengine.Data;
+
+/**
+ * Created by george on 29/04/2017.
+ */
+
+public class BookAsyncTaskLoader {
+}
---
+++ app/src/main/java/com/georgestudenko/bookssearchengine/Util/NetworkUtils.java
@@ -0,0 +1,8 @@
+package com.georgestudenko.bookssearchengine.Util;
+
+/**
+ * Created by george on 29/04/2017.
+ */
+
+public class NetworkUtils {
+}
