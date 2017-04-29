7ff7673c22414c8cf30979b6d9a96769e87642c8
George Studenko <gnstudenko@gmail.com>
2017-04-30 00:54:13 +0200

    feat: Implement MainActivity onLoaderReset

--- app/src/main/java/com/georgestudenko/bookssearchengine/MainActivity.java
+++ app/src/main/java/com/georgestudenko/bookssearchengine/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoaderReset(Loader<List<Book>> loader) {
-
+            mAdapter = null;
+             mListView.setAdapter(mAdapter);
     }
 }
