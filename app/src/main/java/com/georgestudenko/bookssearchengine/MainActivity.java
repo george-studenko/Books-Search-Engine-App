c22ff0faff2c56053318ab31129c1948a40486e1
George Studenko <gnstudenko@gmail.com>
2017-04-29 23:22:54 +0200

    feat: Add searchBooks method and hook to the search button

--- app/src/main/java/com/georgestudenko/bookssearchengine/MainActivity.java
+++ app/src/main/java/com/georgestudenko/bookssearchengine/MainActivity.java
@@ -4,17 +4,35 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.georgestudenko.bookssearchengine.Data.BookAdapter;
+import com.georgestudenko.bookssearchengine.Data.BookAsyncTaskLoader;
 import com.georgestudenko.bookssearchengine.Models.Book;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>>{
 
+    private static final int LOADER_ID = 182;
+    EditText mSearchTerm;
+    Button mSearch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSearchTerm = (EditText) findViewById(R.id.searchText);
+        mMessageText = (TextView) findViewById(R.id.messageText);
+        mSearch = (Button) findViewById(R.id.searchButton);
+    public void searchBooks(View v){
+            Bundle bundle = new Bundle();
+            bundle.putString("searchTerm", mSearchTerm.getText().toString());
+            getSupportLoaderManager().restartLoader(LOADER_ID,bundle,this);
     }
 
     @Override
