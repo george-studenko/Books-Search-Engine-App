d9aa455f63cfcb4ecce19de1403e2a2bc410c5fe
George Studenko <gnstudenko@gmail.com>
2017-04-30 13:49:43 +0200

    feat: Implement missing code on parseBooks

--- app/src/main/java/com/georgestudenko/bookssearchengine/Util/BookParser.java
+++ app/src/main/java/com/georgestudenko/bookssearchengine/Util/BookParser.java
@@ -1,6 +1,9 @@
 package com.georgestudenko.bookssearchengine.Util;
 
+import android.content.Context;
+
 import com.georgestudenko.bookssearchengine.Models.Book;
+import com.georgestudenko.bookssearchengine.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,14 +12,12 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.georgestudenko.bookssearchengine.R.id.authors;
-
 /**
  * Created by george on 29/04/2017.
  */
 
 public class BookParser {
-    public static List<Book> parseBooks(String json){
+    public static List<Book> parseBooks(String json, Context context){
         List<Book> books = new ArrayList<>();
         try {
             JSONObject root = new JSONObject(json);
@@ -30,19 +31,25 @@ public class BookParser {
 
                 String title = (volumeInfo.has("title") ? volumeInfo.getString("title") : "");
                 String subTitle = (volumeInfo.has("subTitle") ? volumeInfo.getString("subtitle") : "");
-                StringBuilder authorBuilder =  new StringBuilder();
-                JSONArray authors = volumeInfo.getJSONArray("authors");
 
-                for(int n = 0 ; n < authors.length(); n++){
-                    authorBuilder.append(authors.get(n));
-                    if(n+1 != authors.length()){
-                        authorBuilder.append(", ");
+                String author = "Unknown";
+                StringBuilder authorBuilder =  new StringBuilder();
+
+                if(volumeInfo.has("authors")) {
+                    JSONArray authors = volumeInfo.getJSONArray("authors");
+
+                    for (int n = 0; n < authors.length(); n++) {
+                        authorBuilder.append(authors.get(n));
+                        if (n + 1 != authors.length()) {
+                            authorBuilder.append(", ");
+                        }
                     }
+                    author = authorBuilder.toString();
                 }
-                String author = authorBuilder.toString();
+
                 String pages = (volumeInfo.has("pageCount") ? volumeInfo.getString("pageCount") : "");
                 String thumbnail = (imageLinks.has("thumbnail")) ? imageLinks.getString("thumbnail") : (imageLinks.has("smallThumbnail") ? imageLinks.getString("smallThumbnail") : "");
-                String shortDescription = (volumeInfo.has("description") ? volumeInfo.getString("description").length()>150 ? volumeInfo.getString("description").substring(0,150)+"..." : volumeInfo.getString("description") : "Description not available");
+                String shortDescription = (volumeInfo.has("description") ? volumeInfo.getString("description").length()>150 ? volumeInfo.getString("description").substring(0,150)+"..." : volumeInfo.getString("description") : context.getString(R.string.description_not_available));
 
                 String buyLink = null;
                 String currency = null;
@@ -53,11 +60,50 @@ public class BookParser {
                     currency = reatailPriceJson.getString("currencyCode").equals("EUR") ? "€" : reatailPriceJson.getString("currencyCode") ;
                     retailPrice = reatailPriceJson.getString("amount");
                     buyLink = saleInfo.getString("buyLink");
-                }else{
-
                 }
 
-                Book book = new Book(title,subTitle,author,shortDescription,thumbnail,pages,buyLink,"#","#",currency,retailPrice);
+                String previewLink = null;
+                boolean previewAvailable = false;
+
+                if(accessInfo.has("epub")) {
+                    JSONObject epub = accessInfo.getJSONObject("epub");
+                    if (epub.has("acsTokenLink")) {
+                        previewAvailable = true;
+                        previewLink = epub.getString("acsTokenLink");
+                    }
+                }
+
+                if(!previewAvailable){
+                    if(accessInfo.has("pdf")) {
+                        JSONObject pdf = accessInfo.getJSONObject("pdf");
+                        if (pdf.has("acsTokenLink")) {
+                            previewAvailable = true;
+                            previewLink = pdf.getString("acsTokenLink");
+                        }
+                    }
+                }
+
+                if(!previewAvailable){
+                    if(accessInfo.has("webReaderLink")) {
+                            previewAvailable = true;
+                            previewLink = accessInfo.getString("webReaderLink");
+                    }
+                }
+
+                if(!previewAvailable){
+                    if(volumeInfo.has("previewLink")) {
+                            previewAvailable = true;
+                            previewLink = volumeInfo.getString("previewLink");
+                    }
+                }
+
+                String infoLink = null;
+                    if(volumeInfo.has("infoLink")) {
+                        infoLink = volumeInfo.getString("infoLink");
+                    }
+
+
+                Book book = new Book(title,subTitle,author,shortDescription,thumbnail,pages,buyLink,previewLink,infoLink,currency,retailPrice);
                 if(book !=null) {
                     books.add(book);
                 }
