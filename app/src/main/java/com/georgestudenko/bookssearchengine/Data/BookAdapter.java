1385de86297dd1b25f914e46303f9219e8035ef1
George Studenko <gnstudenko@gmail.com>
2017-04-30 13:29:34 +0200

    refactor: Extract strings from BookAdapter

--- app/src/main/java/com/georgestudenko/bookssearchengine/Data/BookAdapter.java
+++ app/src/main/java/com/georgestudenko/bookssearchengine/Data/BookAdapter.java
@@ -3,7 +3,6 @@ package com.georgestudenko.bookssearchengine.Data;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -62,7 +61,7 @@ public class BookAdapter extends ArrayAdapter<Book> {
         }
 
         if(currentBook.getPages().length()>0) {
-            pages.setText(currentBook.getPages() + " pages");
+            pages.setText(currentBook.getPages() + mContext.getString(R.string.pages));
         }else{
             pages.setVisibility(View.GONE);
         }
@@ -72,7 +71,7 @@ public class BookAdapter extends ArrayAdapter<Book> {
 
         if(currentBook.getRetailPrice()!=null){
             buy.setVisibility(View.VISIBLE);
-            buy.setText("Buy "+currentBook.getRetailPrice()+" "+ currentBook.getCurrency());
+            buy.setText(mContext.getString(R.string.buy)+currentBook.getRetailPrice()+" "+ currentBook.getCurrency());
 
             buy.setOnClickListener(new View.OnClickListener() {
                 @Override
